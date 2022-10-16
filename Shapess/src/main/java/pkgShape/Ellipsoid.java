c62862b47147319b1a3d2f7cad68dc62f82cf41a
makaylpham <makaylpham@localhost>
2022-10-16 18:21:23 -0400

    initial commit

---
+++ Shapess/src/main/java/pkgShape/Ellipsoid.java
@@ -0,0 +1,5 @@
+package pkgShape;
+
+public abstract class Ellipsoid {
+
+}
---
+++ Shapess/src/test/java/pkgShape/TestCircle.java
@@ -0,0 +1,14 @@
+package pkgShape;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCircle {
+
+	@Test
+	public void TestAsserTrue() {
+		assertTrue(1==1);
+	}
+
+}
